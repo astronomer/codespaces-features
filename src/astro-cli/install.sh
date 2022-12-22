ae4c751b9bea91ccf5276356e2f57a459e8cf5c0
Faisal <fhoda@users.noreply.github.com>
2022-12-22 17:39:19 -0600

    sudo case handling

--- src/astro-cli/install.sh
+++ src/astro-cli/install.sh
@@ -5,14 +5,13 @@ VERSION=${VERSION:-"latest"}
 
 echo "[Astronomer] Installing astro-cli:${VERSION}"
 
-sudo apt-get update -y
-
-sudo apt-get install curl -y
-
-
 if [ "$(id -u)" != 0 ]; then
+    apt-get update -y
+    apt-get install curl -y
     curl -sSL install.astronomer.io | sudo bash -s -- $VERSION
 else
+    apt-get update -y
+    apt-get install curl -y
     curl -sSL install.astronomer.io | bash -s -- $VERSION
 fi
 
